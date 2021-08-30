8451e7e38886eec9fcbf3a24ba1c2b2aeef6dd26
Evgeniy Kotmyshev <77634691+kotmyshev@users.noreply.github.com>
2021-08-30 18:47:43 +0400

    Update net_http_client_local.go

--- net_http_client_local.go
+++ net_http_client_local.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"net/http"
 
-	"bytes"
+	"net/url"
 	"time"
 )
 
@@ -19,27 +19,27 @@ func main() {
 	//request, err := http.NewRequest("GET", "http://localhost:8080", nil)
 	//r := strings.NewReader("Hello, Reader!!!!!!!!!!!!!!!dv")
 
-	message := map[string]interface{}{
-		"hello": "world",
-		"life":  42,
-		"embedded": map[string]string{
-			"yes": "of course!",
-		},
-	}
-
-	bytesRepresentation, err := json.Marshal(message)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
+	v := url.Values{}
+	v.Set("hello", "Говно")
+	v.Add("life", "Какашка")
+	v.Add("friend", "Sarah")
+	v.Add("friend", "Zoe")
 
 	//response, err := client.Post("http://localhost:8080", "text/plain", bytes.NewBufferString("Hello, Reader!!!!!!!!!!!!!!!dv"))
-	response, err := client.Post("http://localhost:8080", "application/json", bytes.NewBuffer(bytesRepresentation))
-	//client.PostForm()
+	//response, err := client.Post("http://localhost:8080", "application/json", bytes.NewBuffer(bytesRepresentation))
+	response, err := client.PostForm("http://localhost:8080", v)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(response.StatusCode)
+	//fmt.Println(response.Write())
+	//fmt.Println(response.Body)
 
+	var result map[string]interface{}
+
+	json.NewDecoder(response.Body).Decode(&result)
+
+	log.Println(result)
+	log.Println(result["hello"])
 }
